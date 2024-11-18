8d96d72c6b727cd4d3cd595d8ae7c486e642c880
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-11-18 13:34:48 +0530

    frontend - backend socket connection

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+
 import './Chat.css';
 
+let socket;
+
 const Chat = () => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const ENDPOINT = "localhost:4000";
+
+  useEffect(() => {
+    const {name, room} = queryString.parse(window.location.search);
+
+    socket = io(ENDPOINT);
+    console.log(socket);
+
+    setName(name);
+    setRoom(room);
+
+    
+  }, [ENDPOINT, window.location.search]);
+
   return (
     <div>
       chat
--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 const router = require('./router');
 
 const PORT = process.env.PORT || 4000;
-
 const app  = express();
+
+app.use(cors());
+
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {
+    cors: {
+        origin: 'http://localhost:3000', // Your front-end URL
+        methods: ['GET', 'POST'],
+    },
+});
 
 io.on('connection', (socket) => {
     console.log('new connection!!!');
