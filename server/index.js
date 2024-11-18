f75edc12c7f1bab5b040c61a72e7d62f4e146606
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-11-18 12:57:39 +0530

    UI for join(home page) completed

--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const router = require('./router');
 
 const PORT = process.env.PORT || 4000;
 
 const app  = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log('new connection!!!');
+    socket.on('disconnect', () => {
+        console.log('user left');
+    });
+});
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
