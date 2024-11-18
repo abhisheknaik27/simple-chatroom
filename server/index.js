096bc6a309db6706c500875a2358689890d501c8
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-11-18 16:07:32 +0530

    server socket userid, initial message

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const socketio = require('socket.io');
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./user');
+
 const http = require('http');
 const cors = require('cors');
 const router = require('./router');
@@ -18,12 +21,25 @@ const io = socketio(server, {
 });
 
 io.on('connection', (socket) => {
-    console.log('new connection!!!');
-
     socket.on('join', ({ name, room }, callback) => {
-        console.log(name, room);
-        const error = true; 
-    })
+        const { error, user } = addUser({ id: socket.id, name, room });
+        if(error) return callback(error);
+
+        socket.emit('message', { user: 'admin', text: `Welcome ${user.name} to Room ${user.room}`});
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined`});
+        socket.join(user.room);
+
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message });
+
+        callback();
+    });
+
     socket.on('disconnect', () => {
         console.log('user left');
     });
