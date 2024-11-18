91fe90ca6420cbb4259426e980e82c1f3856cbbb
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-11-18 12:13:43 +0530

    initialized client (router added)

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Join from './components/Join';
+import Chat from './components/Chat';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' exact Component={Join} />
+        <Route path='/chat' Component={Chat}/>
+      </Routes>
+      </Router>
+  )
+}
+
+export default App
